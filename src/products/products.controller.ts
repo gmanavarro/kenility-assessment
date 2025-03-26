bc79160e15ce2f006fdcd67edffb9ff42fb05336
German Navarro <g.manavarro@gmail.com>
2025-03-26 20:13:21 -0300

    feat: apply authentication guard to product endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,14 +5,17 @@ import {
   Param,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ImageValidatorInterceptor } from './interceptors/image-validator.interceptor';
 import { ProductsService } from './products.service';
 
 @Controller('products')
+@UseGuards(JwtAuthGuard)
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
